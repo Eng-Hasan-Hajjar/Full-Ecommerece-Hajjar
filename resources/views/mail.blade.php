4ba4a6623f53cfab8a46f4efe531c1c6c727e277
Eng-Hasan-Hajjar <eng.hasan.hajjar@gmail.com>
2022-09-13 16:59:22 -0700

    update the Mail  page

--- resources/views/mail.blade.php
+++ resources/views/mail.blade.php
@@ -8,23 +8,23 @@
 
  <div class="row">
             <div class="col-md-12">
-                <h1>Холбоо барих</h1>
+                <h1>to contact</h1>
                 <hr>
                 <form action="{{ url('contact') }}" method="POST">
                     {{ csrf_field() }}
                     <div class="form-group">
-                        <label name="email">Мэйл хаяг:</label>
+                        <label name="email">Email address:</label>
                         <input id="email" name="email" class="form-control">
                     </div>
 
                     <div class="form-group">
-                        <label name="subject">Гарчиг:</label>
+                        <label name="subject">Title:</label>
                         <input id="subject" name="subject" class="form-control">
                     </div>
 
                     <div class="form-group">
-                        <label name="message">Зурвас:</label>
-                        <textarea id="message" name="message" class="form-control">Зурвас бичнэ үү...</textarea>
+                        <label name="message">Message:</label>
+                        <textarea id="message" name="message" class="form-control">Please write a message...</textarea>
                     </div>
 
                     <input type="submit" value="Send Message" class="btn btn-success">
